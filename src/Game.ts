b9f4aea9c524f3577b8b64b9e4fb9aec72b53496
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-02-18 04:41:02 +0530

    refactor: add constants for magic numbers

--- src/Game.ts
+++ src/Game.ts
@@ -3,19 +3,22 @@ export class Game {
     currentRoll: number = 0;
     currentFrame: number = 0;
 
+    readonly maxFrames: number = 10;
+    readonly rollsInFrame: number = 2;
+
     public score(): number {
         return this.currentScore;
     }
 
     roll(knockedPinsCount: number) {
-        if (this.currentFrame >= 10) {
+        if (this.currentFrame >= this.maxFrames) {
             throw new Error('Game over. Cannot roll more than 20 times.');
         }
 
         this.currentScore += knockedPinsCount;
         this.currentRoll++;
 
-        if (this.currentRoll === 2) {
+        if (this.currentRoll === this.rollsInFrame) {
             this.currentFrame++;
             this.currentRoll = 0;
         }
